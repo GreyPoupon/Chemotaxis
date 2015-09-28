d028805e105246103471b677ee2e118c8f3a9c0d
unknown <ivkwong1@697-334-Nogrod.sf.k12.internal>
2015-09-28 07:48:50 -0700

    life and death

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,7 +1,23 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
  Dead [] plague;
  Living man = new Living(250, 250);
  boolean alive;
- void setup()   
+ public void setup()   
  {     
  	background(0);
  	size(500,500);
@@ -12,7 +28,7 @@
  		plague[i] = new Dead((int)(Math.random()*491), (int)(Math.random()*491));
  	} 
  }   
- void draw()   
+ public void draw()   
  {   
  	fill(0, 25);
  	rect(-25, -25, 550, 550);
@@ -26,7 +42,7 @@
  		plague[i].kill();
  	} 
  }
- void mousePressed()
+ public void mousePressed()
  {
  	alive = true;
  	man = new Living(mouseX, mouseY);
@@ -39,7 +55,7 @@
  		myX = x;
  		myY = y;
  	}
- 	void walk()
+ 	public void walk()
  	{
  		int randomX = (int)(Math.random()*7) - 3;
  		int randomY = (int)(Math.random()*7) - 3;
@@ -83,7 +99,7 @@
  		muhX = x;
  		muhY = y;
  	}
- 	void spookyWalk()
+ 	public void spookyWalk()
  	{
  		int randomX = (int)(Math.random()*7) - 3;
  		int randomY = (int)(Math.random()*7) - 3;
@@ -140,7 +156,7 @@
  			}
  		}
  	} 
- 	void kill()
+ 	public void kill()
  	{
  		if(muhX == man.myX && muhY == man.myY)
  		{
@@ -148,3 +164,12 @@
  		}
  	} 
  }    
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
